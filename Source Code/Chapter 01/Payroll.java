4bc8f91e3d2dde582dd1fab5d6157b3d7b80fdd5
mjahmad <mahmad2@mix.wvu.edu>
2023-09-05 20:52:55 -0400

    executed some files in cha 01 and ch02

--- Source Code/Chapter 01/Payroll.java
+++ Source Code/Chapter 01/Payroll.java
@@ -1,11 +1,15 @@
-public class Payroll
+ public class Payroll
 {
    public static void main(String[] args)
    {
-      int hours = 40;
-      double grossPay, payRate = 25.0;
-      
-      grossPay = hours * payRate;
-      System.out.println("Your gross pay is $" + grossPay);
+      double hours = 40.73;
+      int grossPay, payRate = 25;      
+      grossPay = (int) (hours * payRate);
+      System.out.println("Your gross pay is $\n" + grossPay);
+      System.out.print("The numebr of hours is\t\t\t\t\t\t\t"+ hours);
+      System.out.print("\n"+1.0/2);
+      System.out.print("\t\\t");
+
+
    }
 }
--- Source Code/Chapter 02/Variable.java
+++ Source Code/Chapter 02/Variable.java
@@ -4,10 +4,11 @@ public class Variable
 {
    public static void main(String[] args)
    {
-      int value;
+      int $_value$90;
 
-      value = 5;
-      System.out.print("The value is ");
-      System.out.println(value);
+      $_value$90 = 5;
+      System.out.print("The value = ");      
+      System.out.println($_value$90);
+
    }
 }
